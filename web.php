817a1fba3b7a5d0737b69a065c1420bb2baa30f3
triwahyudi363 <yuyud.wahyudi363@gmail.com>
2019-03-22 09:57:51 +0700

    publish file web

--- web.php
+++ web.php
@@ -11,6 +11,43 @@
 |
 */
 
-Route::get('/halo', function () {
-    return "hello world welcome";
+Route::get('/', function () {
+    return view ('welcom');
 });
+ 
+
+
+Route::get('/halo', function () {
+    return "hello !!! welcome";
+});
+
+
+Route::get('/show/{id}', function ($id) {
+    return "Nilai Parameter Adalah :".$id;
+});
+
+Route::get('/show/{id?}', function ($id=1) {
+    return "Nilai Parameter Adalah :".$id;
+});
+
+route::get('/post', 'PostController@index'); 
+route::get('post/{id}' , ['as'=> 'post.detail', function($id){
+echo "post $id";
+echo "</br>";
+echo "body post in ID $id";
+}]);
+
+Route::get('/edit/{nama}', function ($nama) {
+	return "nama saya adalah : " .$nama;
+}) ->where('nama','[A-Za-z]+');
+
+route::get('/index', function(){
+	return "<a href= '" .route('create')."'> akses route dengan nama </a?";
+});
+
+route::get('/create', function () {
+	return "route diakses menggunakan nama";
+
+}) ->name('create');
+
+
